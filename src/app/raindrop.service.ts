393b9ae03751d2c95347d9dfe357d9d004d471ef
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-04-25 20:54:08 +0300

    save token in localstorage

--- src/app/raindrop.service.ts
+++ src/app/raindrop.service.ts
@@ -36,6 +36,18 @@ export class RaindropService {
   access_token: string = "";
 
   getToken(code) {
+    const getColls = this.getCollections.bind(this);
+    const getUser = this.getUser.bind(this);
+    const setToken = (token) => {
+      this.access_token = token;
+    } 
+    const existingToken = localStorage.getItem("raindropToken");
+    if (existingToken) {
+      setToken(existingToken);
+      getUser();
+      getColls();
+      return;
+    }
     const body = {
       code: code,
       client_id: this.clientID,
@@ -43,17 +55,13 @@ export class RaindropService {
       client_secret: "7cf15cbe-222a-462b-8c35-db5933ee4ac1",
       grant_type: "authorization_code",
     };
-    const bodyJSON = JSON.stringify(body);
-    const setToken = (token) => {
-      this.access_token = token;
-    }        
-    const getColls = this.getCollections.bind(this);
-    const getUser = this.getUser.bind(this);
+    const bodyJSON = JSON.stringify(body);          
     const xhttp = new XMLHttpRequest();  
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         const resp = JSON.parse(this.responseText);
         setToken(resp.access_token);
+        localStorage.setItem("raindropToken", resp.access_token);
         getUser();
         getColls();        
       }
@@ -94,27 +102,7 @@ export class RaindropService {
 
   checkCollections(resp) {
     const coll = resp.items.find((el) => el.title === "ImageFinder");
-    /*
-    access: {for: 632978, level: 4, root: false, draggable: true}
-    author: true
-    count: 0
-    cover: []
-    created: "2021-04-25T11:03:46.617Z"
-    creatorRef: {_id: 632978, fullName: "Aleksey Malakhov", name: "aleksey-malakhov-eng"}
-    description: ""
-    expanded: true
-    lastAction: "2021-04-25T11:03:46.617Z"
-    lastUpdate: "2021-04-25T11:03:46.617Z"
-    public: false
-    slug: "image-finder"
-    sort: 17793937
-    title: "ImageFinder"
-    user: {$ref: "users", $id: 632978, $db: ""}
-    view: "list"
-    _id: 17793937
-    */
     if (coll) {
-      console.log("we have it");
       this.getCollection(coll._id);
     } else {
       this.createCollection();
@@ -147,6 +135,7 @@ export class RaindropService {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         const images = JSON.parse(this.responseText).items;
+        console.log(images);
         setImages(images);
       }
     };
