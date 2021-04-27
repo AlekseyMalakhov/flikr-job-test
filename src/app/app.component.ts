149bd4630c5b4b19008d19ad1027c214a8c1c24e
Aleksey Malakhov <hexel@tut.by>
2021-04-27 10:56:05 +0300

    check idle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HostListener, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'flikr-job-test';
+
+  timer;
+
+  ngOnInit() {  
+    this.resetTimer();
+  }
+
+  @HostListener('document:mousemove')
+  @HostListener('document:keypress')
+  @HostListener('document:click')
+  @HostListener('document:wheel')
+  @HostListener('document:touchstart')
+  resetTimer() {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      console.log("Idle time");
+    }, 60000);
+  }
 }
+
